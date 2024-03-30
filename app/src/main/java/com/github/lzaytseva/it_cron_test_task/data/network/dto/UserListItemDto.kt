9d2c0b96723e4019c667bc63838b17e692c951fc
ledrabbit <lvzaytseva1@gmail.com>
2024-03-30 21:52:28 +0300

    removed unnecessary annotation

--- app/src/main/java/com/github/lzaytseva/it_cron_test_task/data/network/dto/UserListItemDto.kt
+++ app/src/main/java/com/github/lzaytseva/it_cron_test_task/data/network/dto/UserListItemDto.kt
@@ -1,9 +1,7 @@
 package com.github.lzaytseva.it_cron_test_task.data.network.dto
 
 import com.google.gson.annotations.SerializedName
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class UserListItemDto(
     @SerializedName("avatar_url") val avatarUrl: String,
     val id: Int,
