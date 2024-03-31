958362e3029d42fac4ce5d7804310ae690c814ad
ledrabbit <lvzaytseva1@gmail.com>
2024-03-31 16:31:43 +0300

    removed unused imports

--- app/src/main/java/com/github/lzaytseva/it_cron_test_task/data/network/api/GithubApiService.kt
+++ app/src/main/java/com/github/lzaytseva/it_cron_test_task/data/network/api/GithubApiService.kt
@@ -1,13 +1,11 @@
 package com.github.lzaytseva.it_cron_test_task.data.network.api
 
 
-import com.github.lzaytseva.it_cron_test_task.BuildConfig
 import com.github.lzaytseva.it_cron_test_task.data.network.dto.UserDto
 import com.github.lzaytseva.it_cron_test_task.data.network.dto.UserListItemDto
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
